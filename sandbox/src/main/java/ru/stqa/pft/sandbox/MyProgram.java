acd267d5db31516b0fbf29b0b2806f2702a9bf8e
Anzhela Povanda <Anzhela.Povanda@dataart.com>
2018-04-17 16:55:02 +0300

    added new change

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
@@ -3,6 +3,26 @@ package ru.stqa.pft.sandbox;
 public class MyProgram {
 	public static void main(String[] args) {
 
-		System.out.println("Hello, Angelica");
+		hello("Angelica");
+		hello("Dima");
+
+		double l = 5;
+    System.out.println("площадь квадрата со стороной" + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("площадь квадрата со сторонами" + a + " & " + b + " = " + area(a,b));
 	}
+
+	public static void hello (String one) {
+    System.out.println("Hello," + one + "!");
+  }
+
+  public static double area (double l) {
+	  return l * l;
+  }
+
+  public static double area (double a, double b) {
+	  return a * b;
+  }
 }
